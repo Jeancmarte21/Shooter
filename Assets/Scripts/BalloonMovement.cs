2639379b9bb33b942bfde07822abf52d2b01b99d
jmlemoine <jeanmelvinlp27@gmail.com>
2020-11-21 08:45:27 -0400

    Generando globos aletoriamente

---
+++ Assets/Scripts/BalloonMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonMovement : MonoBehaviour
+{
+
+    public float Velocidad = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position += transform.up * Velocidad * Time.deltaTime;
+
+        //transform.position -= transform.up * Velocidad * Time.deltaTime;
+        
+    }
+}
---
+++ Assets/Scripts/GloboAm_Creator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GloboAm_Creator : MonoBehaviour
+{
+
+    public GameObject GlobosAm;
+    public float tiempoCreacion = 2f, RangoCreacion = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("CreandoAm", 0.0f, tiempoCreacion);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreandoAm()
+    {
+        Vector3 SpawnPosition = new Vector3(0, 0, 0);
+        SpawnPosition = this.transform.position + Random.onUnitSphere * RangoCreacion;
+        SpawnPosition = new Vector3(SpawnPosition.x, this.transform.position.y, 0);
+
+        GameObject GloboAm = Instantiate(GlobosAm, SpawnPosition, Quaternion.identity);
+    }
+
+}
